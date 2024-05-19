5c97d20e1a0c93326b08c256736e63df7f485424
yourSrijit <berasrijit02@gmail.com>
2024-05-19 23:08:09 +0530

    Update

--- React with Projects/Projects/Project1/src/App.jsx
+++ React with Projects/Projects/Project1/src/App.jsx
@@ -5,7 +5,16 @@ function App() {
 
 
   return (
-     <div className='bg-red-500'>Hi srijit</div>
+     <div className='w-screen h-screen'>
+     
+     <nav className='w-[15%] h-full bg-zinc-100'>
+      <a href="/create">Add New product</a>
+      <h1>Catagory</h1>
+      <ul>
+        <li>Cat</li>
+      </ul>
+     </nav>
+     </div>
   )
 }
 
