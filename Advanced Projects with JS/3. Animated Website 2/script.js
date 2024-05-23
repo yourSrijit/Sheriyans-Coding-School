fe2b48991fa88c8950b5ad4e3caf3cd823532808
yourSrijit <berasrijit02@gmail.com>
2024-05-23 11:37:05 +0530

    Footer Section complete with colour animation

--- Advanced Projects with JS/3. Animated Website 2/script.js
+++ Advanced Projects with JS/3. Animated Website 2/script.js
@@ -2,3 +2,27 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
+
+let fixed=document.querySelector("#fixed-image");
+let elmContainer=document.querySelector("#element-container")
+let elm=document.querySelectorAll(".element");
+
+
+
+elm.forEach((e)=>{
+    e.addEventListener("mouseenter",()=>{
+        let image=e.getAttribute("image-data");
+        fixed.style.display="block";
+        fixed.style.backgroundImage=`url(${image})`
+     })
+})
+elmContainer.addEventListener("mouseleave",()=>{
+    fixed.style.display="none";
+})
+
+
+
+
+
