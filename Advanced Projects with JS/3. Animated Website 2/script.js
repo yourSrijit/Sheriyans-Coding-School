697175a7f4bd2d47aa5a64db82eff0a7267545ef
yourSrijit <berasrijit02@gmail.com>
2024-05-24 23:17:31 +0530

    Animated Cursor complete with Swipe js component

--- Advanced Projects with JS/3. Animated Website 2/script.js
+++ Advanced Projects with JS/3. Animated Website 2/script.js
@@ -5,24 +5,66 @@ const scroll = new LocomotiveScroll({
 
 
 
-let fixed=document.querySelector("#fixed-image");
-let elmContainer=document.querySelector("#element-container")
-let elm=document.querySelectorAll(".element");
+function page4Animation(){
+    let fixed=document.querySelector("#fixed-image");
+    let elmContainer=document.querySelector("#element-container")
+    let elm=document.querySelectorAll(".element");
 
 
-
-elm.forEach((e)=>{
-    e.addEventListener("mouseenter",()=>{
-        let image=e.getAttribute("image-data");
+    fixed.addEventListener("mouseenter",()=>{
         fixed.style.display="block";
-        fixed.style.backgroundImage=`url(${image})`
-     })
+        fixed.style.cursor="pointer";
+    })
+    elm.forEach((e)=>{
+        e.addEventListener("mouseenter",()=>{
+            let image=e.getAttribute("image-data");
+            fixed.style.display="block";
+            fixed.style.backgroundImage=`url(${image})`
+        })
+    })
+    elmContainer.addEventListener("mouseleave",()=>{
+        fixed.style.display="none";
+    })
+
+}
+
+function swiperAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+      });
+}
+
+swiperAnimation();
+page4Animation();
+
+
+
+// Animated Cursor 
+let cursor=document.querySelector("#animated-cursor");
+let page4=document.querySelector("#page4");
+
+page4.addEventListener("mouseenter",(e)=>{
+    page4.style.cursor="none"
+
+
+    cursor.style.display="flex"
+    cursor.style.alignItems="center"
+    cursor.style.justifyContent="center"
+
 })
-elmContainer.addEventListener("mouseleave",()=>{
-    fixed.style.display="none";
+
+page4.addEventListener("mousemove",(e)=>{
+   const x=e.pageX;
+   const y=e.pageY;
+   console.log(e);
+   cursor.style.left=`${x}px`;
+   cursor.style.top=`${y-300}px`;
+
 })
 
-
-
-
-
+page4.addEventListener("mouseleave",(e)=>{
+    page4.style.cursor="default"
+    cursor.style.display="none";
+})
